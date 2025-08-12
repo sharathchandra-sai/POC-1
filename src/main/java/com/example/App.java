5f09b1d5761776e3f3e7642cc4bfb7728d565e66
sharathchandra-sai <kodati.sharath2000@gmail.com>
2025-08-12 13:25:30 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,19 @@
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
